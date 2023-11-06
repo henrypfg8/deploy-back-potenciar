a24c27af10ef29d0a7c30d40463874df12d56172
enzosamojedny <enzosamojedny@gmail.com>
2023-11-06 01:34:17 -0300

    agregado front

--- src/app.js
+++ src/app.js
@@ -32,7 +32,8 @@ server.use(cokieParser());
 server.use(morgan("dev"));
 server.use((_req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin"
+    "Access-Control-Allow-Origin",
+    'https://potenciar-solidario.vercel.app/'
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
