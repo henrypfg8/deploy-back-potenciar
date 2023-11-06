d6f089efdbe004909aa5ef545e41956f1ffa7e28
enzosamojedny <enzosamojedny@gmail.com>
2023-11-06 01:03:47 -0300

    please work

--- src/app.js
+++ src/app.js
@@ -32,9 +32,7 @@ server.use(cokieParser());
 server.use(morgan("dev"));
 server.use((_req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin",
-    "http://localhost:5432",
-    "http://localhost:5173"
+    "Access-Control-Allow-Origin"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
