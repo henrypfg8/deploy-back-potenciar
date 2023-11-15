9fc8d242cb615c1836c5add2732dadaef0162aa6
enzosamojedny <enzosamojedny@gmail.com>
2023-11-14 21:20:49 -0300

    ultimos cambios (dencerio)

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ server.use((_req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "http://localhost:19789",
-    "http://localhost:5173"
+    'https://potenciar-solidario.vercel.app/'
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
--- src/db.js
+++ src/db.js
@@ -5,14 +5,15 @@ const fs = require("fs");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 // conexion con a tu base de datos
-const sequelize = new Sequelize({
-  host: DB_HOST,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_NAME,
-  dialect: "mysql", 
-  logging:false
+const sequelize = new Sequelize('postgres://potenciar_solidario_user:6EgUYdMzl0hT2nb1IZGH7oQoouYtrH2e@dpg-cl45um3iu76s73b5pvb0-a.oregon-postgres.render.com/potenciar_solidario',{
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  ssl:true,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false, // This is required if using a self-signed certificate
+    },
+  },
 });
 
 const basename = path.basename(__filename);
